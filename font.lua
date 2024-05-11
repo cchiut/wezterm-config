881f03251a7f31b0c1608d39bde2872cb97811d7
cchiut <668280+cchiut@users.noreply.github.com>
2024-05-11 11:22:32 +0800

    add nerd font

--- font.lua
+++ font.lua
@@ -3,7 +3,9 @@ local wezterm = require('wezterm')
 local module = {}
 
 function module.apply_to_config(config)
-  config.font_size = 14.0
+  config.font = wezterm.font('CodeNewRoman Nerd Font')
+  config.font_size = 14.8
+  config.line_height = 1.1
   config.use_cap_height_to_scale_fallback_fonts = true
 end
 
