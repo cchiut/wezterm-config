bf6fc90e75b7609fa159213e41c35c84be15f972
cchiut <668280+cchiut@users.noreply.github.com>
2024-05-11 18:42:43 +0800

    make font wirh fallback

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,6 @@ local config = wezterm.config_builder()
 -- window
 config.color_scheme = 'Tokyo Night (Gogh)'
 config.text_background_opacity = 0.9
-config.window_background_opacity = 0.9
 config.window_padding = {
   left = '0.5cell',
   right = '0.5cell',
@@ -14,10 +13,27 @@ config.window_padding = {
 }
 
 -- font
+config.font = wezterm.font_with_fallback {
+  'CodeNewRoman Nerd Font',
+  'JetBrains Mono',
+  'Noto Color Emoji',
+  { family = 'Symbols Nerd Font Mono', scale = 0.75 }
+}
+config.line_height = 1.1
 config.use_cap_height_to_scale_fallback_fonts = true
 
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+  config.window_background_opacity = 0.9
+
+  -- font
+  config.font_size = 14.8
+end
+
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  -- font
   config.font_size = 11
+
+  -- WSL
   config.wsl_domains = {
     {
       name = 'WSL:Debian',
@@ -27,12 +43,5 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 
   config.default_domain = 'WSL:Debian'
 end
-
-if wezterm.target_triple == 'aarch64-apple-darwin' then
-  config.font = wezterm.font('CodeNewRoman Nerd Font')
-  config.font_size = 14.8
-  config.line_height = 1.1
-end
-
 
 return config
