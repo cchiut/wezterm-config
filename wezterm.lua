dd2005ac78d54e37a2d05d8514ce5e1a98b7876c
cchiut <668280+cchiut@users.noreplay.github.com>
2024-05-11 14:08:44 +0800

    add windows adjustments

--- wezterm.lua
+++ wezterm.lua
@@ -14,9 +14,25 @@ config.window_padding = {
 }
 
 -- font
-config.font = wezterm.font('CodeNewRoman Nerd Font')
-config.font_size = 14.8
-config.line_height = 1.1
 config.use_cap_height_to_scale_fallback_fonts = true
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.font_size = 11
+  config.wsl_domains = {
+    {
+      name = 'WSL:Debian',
+      distribution = 'Debian',
+    }
+  }
+
+  config.default_domain = 'WSL:Debian'
+end
+
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+  config.font = wezterm.font('CodeNewRoman Nerd Font')
+  config.font_size = 14.8
+  config.line_height = 1.1
+end
+
 
 return config
