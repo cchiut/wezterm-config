4ff097b25a58f2f01fdce94945c8690184813cbd
cchiut <668280+cchiut@users.noreply.github.com>
2024-12-23 11:59:12 +0800

    make color schema depends on system

--- wezterm.lua
+++ wezterm.lua
@@ -2,15 +2,34 @@ local wezterm = require('wezterm')
 
 local config = wezterm.config_builder()
 
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Mocha (Gogh)'
+  else
+    return 'Catppuccin Latte (Gogh)'
+  end
+end
+
 -- window
-config.color_scheme = 'Tokyo Night (Gogh)'
-config.text_background_opacity = 0.9
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.window_padding = {
-  left = '0.5cell',
-  right = '0.5cell',
-  top = '2',
-  bottom = '2',
+  left = '1cell',
+  right = '1cell',
+  top = '4',
+  bottom = '4',
 }
+
+-- tab
+config.hide_tab_bar_if_only_one_tab = true
 
 -- font
 config.font = wezterm.font_with_fallback {
@@ -23,10 +42,8 @@ config.line_height = 1.1
 config.use_cap_height_to_scale_fallback_fonts = true
 
 if wezterm.target_triple == 'aarch64-apple-darwin' then
-  config.window_background_opacity = 0.9
-
   -- font
-  config.font_size = 14.8
+  config.font_size = 16
 end
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
